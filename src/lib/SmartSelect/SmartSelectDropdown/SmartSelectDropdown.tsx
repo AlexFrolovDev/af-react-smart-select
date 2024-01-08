aa6d1afa7b177e08dd3d13ee13566bdb37d5bd47
DESKTOP-F5QML8E\Alex <golan4ik@gmail.com>
2024-01-08 17:58:58 +0200

    remove list when closed

--- src/lib/SmartSelect/SmartSelectDropdown/SmartSelectDropdown.tsx
+++ src/lib/SmartSelect/SmartSelectDropdown/SmartSelectDropdown.tsx
@@ -15,39 +15,32 @@ const SmartSelectDropdown: FC<SmartSelectDropdownProps> = (props) => {
       open={open}
       data-testid="SmartSelectDropdownWrapper"
     >
-      <SmartSelectDropdownContent>
-        {/* {options.map((option, index) => (
-          <SmartSelectOption
-            selected={selectedValues.includes(option.value)}
-            onOptionClick={onChange}
-            key={index}
-            label={option.label}
-            value={option.value}
-          />
-        ))} */}
-        {data.map((group) => {
-          return (
-            <>
-              {group.label && (
-                <OptionSeparator key={`group-id-${group.id}`}>
-                  {group.label}
-                </OptionSeparator>
-              )}
-              {group.items.map((item) => {
-                return (
-                  <SmartSelectOption
-                    selected={selectedValues.includes(item.value.toString())}
-                    onOptionClick={onChange}
-                    key={group.id + "" + item.value}
-                    label={item.label}
-                    value={item.value.toString()}
-                  />
-                );
-              })}
-            </>
-          );
-        })}
-      </SmartSelectDropdownContent>
+      {open && (
+        <SmartSelectDropdownContent>
+          {data.map((group) => {
+            return (
+              <>
+                {group.label && (
+                  <OptionSeparator key={`group-id-${group.id}`}>
+                    {group.label}
+                  </OptionSeparator>
+                )}
+                {group.items.map((item) => {
+                  return (
+                    <SmartSelectOption
+                      selected={selectedValues.includes(item.value.toString())}
+                      onOptionClick={onChange}
+                      key={group.id + "" + item.value}
+                      label={item.label}
+                      value={item.value.toString()}
+                    />
+                  );
+                })}
+              </>
+            );
+          })}
+        </SmartSelectDropdownContent>
+      )}
     </SmartSelectDropdownWrapper>
   );
 };
